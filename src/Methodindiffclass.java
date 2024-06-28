b7d83fc3f2ba0d2062b11fea5d937232ffbf92fb
Shubhada <subhadadange@gmail.com>
2024-06-28 23:55:19 +0530

    Architect Asian development

--- src/Methodindiffclass.java
+++ src/Methodindiffclass.java
@@ -4,6 +4,8 @@ public class Methodindiffclass {
 	public String getUserData()
 	{
 		System.out.println("hello world");
+		System.out.println("HEHEHE");
+		System.out.println("HAHAHA");
 		return "Shubhi";
 	}
 }
